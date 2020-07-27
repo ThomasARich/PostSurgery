8b09238ab7c925fe7f04e6a1720080dbf512f3bb
ThomasARich <63765428+ThomasARich@users.noreply.github.com>
2020-07-27 15:09:48 -0700

    Add files via upload

---
+++ PostSurgery/PostSurgery/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  PostSurgery
+//
+//  Created by Thomas Rich on 5/23/20.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+class MedRem: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+class PainLog: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+class EntryForm: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    /* This section of code is still in progress.
+       TODO: implement file save logic
+       TODO: Implement UI conditional for this page
+    
+     bool error = false;
+     str nText, dText, eText = ""
+     
+     @IBAction func NameField(_ sender: UIButton) {
+        let nText = NameField.text!
+     }
+     @IBAction func DOBField(_ sender: DOBField) {
+        let dText = DOBField.text!
+     }
+     @IBAction func EmailField(_ sender: EmailField) {
+        let eText = EmailField.text!     }
+     @IBAction func SubmitForm(_ sender: SubmitForm) {
+     }
+     if (nText == "") {
+        // add name prompt to error
+        // bool error = true
+     }
+     if (DOBField == "") {
+        // add DOB prompt to error
+        // bool error = true
+     }
+     if (EmailField == "") {
+        // add email prompt to error
+        // bool error = true
+     }
+     if (error) {
+        // display error field
+     }
+     else {
+        // save all strings to file. Navigate to next page
+     }
+     /* Check to make sure that each field is filled out. If they are, write to
+        file. Else, show error field that tells the user which field(s) still
+        require info. */
+     */
+}
+    
+class ExerciseRem: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
